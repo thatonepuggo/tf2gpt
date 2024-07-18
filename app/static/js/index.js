61e180fe527761b35946153b03174f0cf332d4ba
thatonepuggo <42193477+thatonepuggo@users.noreply.github.com>
2024-07-18 13:22:00 -0400

    queuelen

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -23,6 +23,9 @@ $(function() {
     let killswitch = $("#killSwitch");
     let autoDisableVoice = $("#autoDisableVoice");
     
+    let lastQueue = [];
+    let lastConsole = "";
+
     let scrolls = 0;
 
     function sendCmd(event) {
@@ -60,6 +63,28 @@ $(function() {
     function autoDisableVoiceChanged() {
         socket.emit("set_auto_disable_voice", autoDisableVoice.is(":checked"));
     }
+
+    function arrayCompare(_arr1, _arr2) {
+        if (
+          !Array.isArray(_arr1)
+          || !Array.isArray(_arr2)
+          || _arr1.length !== _arr2.length
+          ) {
+            return false;
+          }
+        
+        // .concat() to not mutate arguments
+        const arr1 = _arr1.concat().sort();
+        const arr2 = _arr2.concat().sort();
+        
+        for (let i = 0; i < arr1.length; i++) {
+            if (arr1[i] !== arr2[i]) {
+                return false;
+             }
+        }
+        
+        return true;
+    }
         
     aicmd.on("keydown",     {"type": "ai", "message": aicmd},       enterCmd);
     rconcmd.on("keydown",   {"type": "rcon", "message": rconcmd},   enterCmd);
@@ -69,26 +94,31 @@ $(function() {
     autoDisableVoice.on("click", autoDisableVoiceChanged);
 
     socket.on("consoleget", (data) => {
+        if (lastConsole == data) return;
         rconconsole.html(data);
+        lastConsole = data;
         if (scrolls == 0) consoleTopBox.scrollTop(consoleTopBox[0].scrollHeight - consoleTopBox[0].clientHeight);
         scrolls++;
     });
 
     socket.on("queueget", (queue) => {
+        if (arrayCompare(lastQueue, queue)) return;
+        
         $("#queuelength").text(queue.length);
         $("#queue tbody").empty();
-        var markup = "";
-        if (queue.length >= 1) {
-            for (var i = 0; i < queue.length; i++) {
-                var actionHTML = `${actionLink(i, 0, "del")} ` +
-                `Send ${actionLink(i, 1, "up")}/${actionLink(i, 2, "down")} in queue. ` +
-                `Send to ${actionLink(i, 3, "front")}/${actionLink(i, 4, "back")}`;
-                var elem = queue[i];
-                markup += `<tr><td>${elem['username']}</td><td>${elem['message']}</td><td>${actionHTML}</td></tr>`;
-            }
-        } else {
-            markup = "<tr><td colspan=\"3\">the queue is empty</td></tr>"
+        
+        var markup = "";        
+        
+        for (var i = 0; i < queue.length; i++) {
+            var actionHTML = `${actionLink(i, 0, "del")} ` +
+            `Send ${actionLink(i, 1, "up")}/${actionLink(i, 2, "down")} in queue. ` +
+            `Send to ${actionLink(i, 3, "front")}/${actionLink(i, 4, "back")}`;
+            var elem = queue[i];
+            markup += `<tr><td>${elem['username']}</td><td>${elem['message']}</td><td>${actionHTML}</td></tr>`;
         }
+
+        lastQueue = queue;
+
         $("#queue tbody").append(markup);
     });
 
